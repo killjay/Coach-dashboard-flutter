a3d427b830e0d2076640084dcc31c0c1bd0851bc
Mohamed Saajid <saajid@iastate.edu>
2025-11-22 22:17:08 -0600

    Fix CardTheme to CardThemeData for Flutter compatibility

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -121,7 +121,7 @@ class AppTheme {
       ),
       iconTheme: const IconThemeData(color: textPrimary),
     ),
-    cardTheme: CardTheme(
+    cardTheme: CardThemeData(
       elevation: 0,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20),
